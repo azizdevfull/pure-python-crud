e2b1ec9c833ba3bc604b24180784250062ea7fa1
azizdevfull <aziz16110904@gmail.com>
2023-03-07 08:39:47 +0500

    add Get all books

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ import json
 data = [
     {"id": 1, "name": "Robinzon Kruzo", "genre": "Tragedia", "author": "Daniel Defo", "price": 45}
 ]
+
+class Books(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        if self.path == '/books':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("men ishladim")
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, Books)
+    httpd.serve_forever()

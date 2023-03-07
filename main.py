66c148cdad1d882f2810d86ffff0dff4e3724925
azizdevfull <aziz16110904@gmail.com>
2023-03-07 08:34:25 +0500

    create new data

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+
+data = [
+    {"id": 1, "name": "Robinzon Kruzo", "genre": "Tragedia", "author": "Daniel Defo", "price": 45}
+]
